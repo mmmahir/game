43f95aea8404c7277317ba49b22161748b20f5c8
mmmahir <98086110+mmmahir@users.noreply.github.com>
2025-12-11 20:44:53 +0100

    a

---
+++ flappy-bird-rn/App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import Bird from './src/components/Bird';
+import { useEffect, useState } from 'react';
+
+export default function App() {
+  const sreenWidth = Dimensions.get("screen").width;
+  const sreenHeight = Dimensions.get("screen").height;
+  const birdLeft = sreenWidth / 2;
+  const [birdBottom,setBirdBottom] = useState(sreenHeight / 2); 
+  const gravity = 3;
+  let gameTimerId;
+
+  useEffect(() => {
+    if (birdBottom > 0){
+      gameTimerId = setInterval(() => {
+        setBirdBottom((birdBottom) => birdBottom - gravity);
+    },30)
+    }
+
+    return () => {
+      clearInterval(gameTimerId);
+    };
+
+  },[birdBottom]);
+  return (
+    
+
+    <View style={styles.container}>
+      <Bird birdBottom={birdBottom} birdLeft={birdLeft} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
