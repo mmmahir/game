3fb3a14ce3ebd4a0594f320b369ec73f8b471dba
mmmahir <98086110+mmmahir@users.noreply.github.com>
2025-12-16 20:53:36 +0100

    a

--- flappy-bird-rn/App.js
+++ flappy-bird-rn/App.js
@@ -11,6 +11,44 @@ export default function App() {
   const gravity = 3;
   let gameTimerId;
   const birdColor = "#f00";
+  let obstacleTimerId;
+  let obstacleTimerId2;
+  const [obstaclesLeft,setObstaclesLeft] = useState(sreenWidth);
+  const [obstaclesLeft2,setObstaclesLeft2] = useState(sreenWidth + sreenWidth / 2 + 30);
+  const [obstacleNegHeight, setobstacleNegHeight] = useState(0);
+  const [obstacleNegHeight2, setobstacleNegHeight2] = useState(0);
+  const obstacleWidth = 60;
+  const obstacleHeight = 300;
+  let gap = 200;
+  
+  useEffect(() => {
+    if (obstaclesLeft > -60){
+      obstacleTimerId = setInterval(() => {
+        setObstaclesLeft(obstaclesLeft => obstaclesLeft - 5);
+      },30);
+      return () => {
+        clearInterval(obstacleTimerId);
+      };
+    } else {
+      setObstaclesLeft(sreenWidth);
+      setobstacleNegHeight(-Math.random() * 100);
+    };
+  }, [obstaclesLeft])
+
+  useEffect(() => {
+      if (obstaclesLeft2 > -60){
+      obstacleTimerId2 = setInterval(() => {
+        setObstaclesLeft2(obstaclesLeft2 => obstaclesLeft2 - 5);
+      },30);
+      return () => {
+        clearInterval(obstacleTimerId2);
+      };
+    } else {
+      setObstaclesLeft2(sreenWidth);
+      setobstacleNegHeight2(-Math.random() * 100);
+    };
+  }, [obstaclesLeft2])
+
 
   useEffect(() => {
     if (birdBottom > 0){
@@ -29,6 +67,22 @@ export default function App() {
 
     <View style={styles.container}>
       <Bird birdBottom={birdBottom} birdLeft={birdLeft} color={birdColor}/>
+      <Obstacle
+      color={"green"}
+      obstacleWidth = {obstacleWidth}
+      obstacleBottom = {obstacleNegHeight}
+      gap = {gap}
+      obstacleLeft = {obstaclesLeft}
+      >
+      </Obstacle>
+      <Obstacle
+      color={"yellow"}
+      obstacleWidth = {obstacleWidth}
+      obstacleBottom = {obstacleNegHeight2}
+      gap = {gap}
+      obstacleLeft = {obstaclesLeft2}
+      >
+      </Obstacle>
       <StatusBar style="auto" />
     </View>
   );
